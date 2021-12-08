ae19683a389df97f507950984e3dfbdff4f5b180
rokibulslam <rokibulslam6@gmail.com>
2021-12-08 10:50:57 +0600

    responsive

--- src/Components/Pages/Home/ContactMe/ContactMe.js
+++ src/Components/Pages/Home/ContactMe/ContactMe.js
@@ -31,73 +31,75 @@ export const ContactMe = () => {
   };
 
   return (
-    <div className="contact-bg ">
-      <div className=" container">
-        <h1 className="text-center py-5 ">Contact With Me</h1>
-        <div className="row">
-          <div className="col-md-6 p-5">
-            <h1 className="pb-4">
-              <span className="text-color">Contact</span>{" "}
-              <span className="text-white">Info</span>
-            </h1>
-            <div className="d-flex text-white">
-              <FontAwesomeIcon
-                className="contact-icon me-3"
-                icon={faEnvelope}
-                size="2x"
-              />
-              <h4 className="">rokibulislam4151@gmail.com</h4>
+    <div>
+      <div className="contact-bg ">
+        <div className=" container">
+          <h1 className="text-center py-5 ">Contact With Me</h1>
+          <div className="row">
+            <div className="col-md-6 mb-5">
+              <h1 className="pb-4">
+                <span className="text-color">Contact</span>{" "}
+                <span className="text-white">Info</span>
+              </h1>
+              <div className="d-flex text-white">
+                <FontAwesomeIcon
+                  className="contact-icon me-3"
+                  icon={faEnvelope}
+                  size="2x"
+                />
+                <p className="">rokibulislam5141@gmail.com</p>
+              </div>
+              <div className="d-flex text-white">
+                <FontAwesomeIcon
+                  className="contact-icon me-3 "
+                  icon={faPhone}
+                  size="2x"
+                />
+                <h5 className="">+8801735754758</h5>
+              </div>
+              <div className="d-flex text-white">
+                <FontAwesomeIcon
+                  className="contact-icon me-3"
+                  icon={faHome}
+                  size="2x"
+                />
+                <h5 className="">Pabna, Rajshahi, Bangladesh</h5>
+              </div>
             </div>
-            <div className="d-flex text-white">
-              <FontAwesomeIcon
-                className="contact-icon me-3 "
-                icon={faPhone}
-                size="2x"
-              />
-              <h4 className="">+8801735754758</h4>
-            </div>
-            <div className="d-flex text-white">
-              <FontAwesomeIcon
-                className="contact-icon me-3"
-                icon={faHome}
-                size="2x"
-              />
-              <h4 className="">Pabna, Rajshahi, Bangladesh</h4>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="p-5 ">
+            <div className="col-md-6 mb-5">
               <div className="">
-                <Form ref={form} onSubmit={sendEmail}>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label className="text-white">Your Name</Form.Label>
-                    <Form.Control type="text" name="name" required />
-                    <Form.Label className="text-white">
-                      Email address
-                    </Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="name@example.com"
-                      name="email"
-                    />
-                  </Form.Group>
-                  <Form.Group
-                    className="mb-3"
-                    controlId="exampleForm.ControlTextarea1"
-                  >
-                    <Form.Label className="text-white">Message</Form.Label>
-                    <Form.Control
-                      className="input-color"
-                      as="textarea"
-                      name="message"
-                      rows={3}
-                    />
-                  </Form.Group>
-                  <input className="contact-btn" type="submit" value="Send" />
-                </Form>
+                <div className="">
+                  <Form ref={form} onSubmit={sendEmail}>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlInput1"
+                    >
+                      <Form.Label className="text-white">Your Name</Form.Label>
+                      <Form.Control type="text" name="name" required />
+                      <Form.Label className="text-white">
+                        Email address
+                      </Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="name@example.com"
+                        name="email"
+                      />
+                    </Form.Group>
+                    <Form.Group
+                      className="mb-3"
+                      controlId="exampleForm.ControlTextarea1"
+                    >
+                      <Form.Label className="text-white">Message</Form.Label>
+                      <Form.Control
+                        className="input-color"
+                        as="textarea"
+                        name="message"
+                        rows={3}
+                      />
+                    </Form.Group>
+                    <input className="contact-btn" type="submit" value="Send" />
+                  </Form>
+                </div>
               </div>
             </div>
           </div>
